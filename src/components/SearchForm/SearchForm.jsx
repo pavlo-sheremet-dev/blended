885661fba358f9b42050e57449450031d8279b4e
DmytroPod <132753424+DmytroPod@users.noreply.github.com>
2023-11-18 20:07:38 +0200

    formsearch

--- src/components/SearchForm/SearchForm.jsx
+++ src/components/SearchForm/SearchForm.jsx
@@ -4,7 +4,33 @@ import { FiSearch } from 'react-icons/fi';
 import { FormBtn, InputSearch, SearchFormStyled } from './SearchForm.styled';
 
 export class SearchForm extends Component {
+  state = {
+    name: '',
+  };
+  handleChange = event => {
+    this.setState({ name: event.target.value });
+  };
+  handleSubmit = ev => {
+    ev.preventDefault();
+    this.props.getQuery(this.state.name);
+    this.setState({ name: '' });
+  };
+
   render() {
-    return <h2>SearchForm</h2>;
+    return (
+      <SearchFormStyled onSubmit={this.handleSubmit}>
+        <FormBtn type="submit">
+          <FiSearch size="16px" />
+        </FormBtn>
+        <InputSearch
+          onChange={this.handleChange}
+          placeholder="What do you want to write?"
+          name="search"
+          required
+          autoFocus
+          value={this.state.name}
+        />
+      </SearchFormStyled>
+    );
   }
 }
--- src/tabs/Gallery.jsx
+++ src/tabs/Gallery.jsx
@@ -11,9 +11,16 @@ import {
 } from '../components';
 
 export class Gallery extends Component {
+  state = {
+    query: '',
+  };
+  getQuery = formData => {
+    this.setState({ query: formData });
+  };
   render() {
     return (
       <>
+        <SearchForm getQuery={this.getQuery}></SearchForm>
         <Text textAlign="center">Sorry. There are no images ... 😭</Text>
       </>
     );
