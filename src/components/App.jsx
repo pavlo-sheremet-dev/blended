cbfce5b84e0408b294d963203f29ca79f3afb050
pavlo-sheremet-dev <goit.pavel.sheremet@gmail.com>
2023-12-02 17:30:15 +0200

    clear some code

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,12 +5,8 @@ import { CocktailDetail, Cocktails, Home } from "../views";
 
 export const App = () => {
   return (
-    <Routes>
-      <Route path="/" element={<Layout />}>
-        <Route index element={<Home />} />
-        <Route path={routes.COCKTAILS} element={<Cocktails />} />
-        <Route path={routes.COCKTAIL_ID} element={<CocktailDetail />} />
-      </Route>
-    </Routes>
+    <h1 className="text-center font-black text-gray-700 text-4xl mb-10">
+      Home Page
+    </h1>
   );
 };
