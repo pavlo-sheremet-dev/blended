27b7b9182ec448543dfece31f51549d1259f6bec
pavlo-sheremet-dev <goit.pavel.sheremet@gmail.com>
2023-12-02 17:28:54 +0200

    connect fonts

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,16 @@
-import { Navigate, Route, Routes } from 'react-router-dom';
-import { Layout } from '../layout/Layout/Layout';
-import { routes } from '../routes';
-import { CocktailDetail, Cocktails, Home } from '../views';
+import { Navigate, Route, Routes } from "react-router-dom";
+import { Layout } from "../layout/Layout/Layout";
+import { routes } from "../routes";
+import { CocktailDetail, Cocktails, Home } from "../views";
 
 export const App = () => {
   return (
-    <h1 className='text-center font-black text-gray-700 text-4xl mb-10'>
-      Home Page
-    </h1>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Home />} />
+        <Route path={routes.COCKTAILS} element={<Cocktails />} />
+        <Route path={routes.COCKTAIL_ID} element={<CocktailDetail />} />
+      </Route>
+    </Routes>
   );
 };
