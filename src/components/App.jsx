4177eb45145e141278c223c13bc6902d6c5415e3
Nadin2611 <132609243+Nadin2611@users.noreply.github.com>
2023-12-02 19:30:12 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,8 +5,13 @@ import { CocktailDetail, Cocktails, Home } from "../views";
 
 export const App = () => {
   return (
-    <h1 className="text-center font-black text-gray-700 text-4xl mb-10">
-      Home Page
-    </h1>
+    <Routes>
+      <Route path={routes.HOME} element={<Layout />}>
+        <Route index element={<Home />} />
+        <Route path={routes.COCKTAILS} element={<Cocktails />} />
+        <Route path={routes.COCKTAIL_ID} element={<CocktailDetail />} />
+        <Route path="*" element={<Navigate to={routes.HOME} />} />
+      </Route>
+    </Routes>
   );
 };
