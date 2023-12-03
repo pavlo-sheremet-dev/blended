ff0da64fbdfd4e7717df037f94cba90b13bb2d6b
pavlo-sheremet-dev <goit.pavel.sheremet@gmail.com>
2023-12-03 13:48:28 +0200

    disable propTypes eslint rule

--- src/views/Cocktails.jsx
+++ src/views/Cocktails.jsx
@@ -1,13 +1,13 @@
-import { SearchForm } from '../components/SearchForm';
-import { Section } from '../components/Section';
-import { CocktailsList } from '../components/CocktailsList';
-import { Loader } from '../components/Loader';
+import { SearchForm } from "../components/SearchForm";
+import { Section } from "../components/Section";
+import { CocktailsList } from "../components/CocktailsList";
+import { Loader } from "../components/Loader";
 
 export const Cocktails = () => {
   return (
     <>
       <Section>
-        <h1 className='uppercase text-4xl text-gray-600 text-center'>
+        <h1 className="uppercase text-4xl text-gray-600 text-center">
           Search Cocktails
         </h1>
 
