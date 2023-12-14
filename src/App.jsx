b688793b56f4929c9feb85a132f88052530d38fa
KaterynaPryimakova <134732205+KaterynaPryimakova@users.noreply.github.com>
2023-12-14 13:42:28 +0200

    task4

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,7 @@ import {
 import data from './data/data.json';
 import article from './data/article.json';
 import forbes from './data/forbes.json';
+import transactions from './data/transactions.json';
 
 export const App = () => {
   return (
@@ -41,7 +42,7 @@ export const App = () => {
         <Heading marginTop="50px" marginBottom="50px" textAlign="center">
           Task 4
         </Heading>
-        <CryptoHistory />
+        <CryptoHistory items={transactions} />
       </Container>
     </Section>
   );
--- src/components/CryptoHistory/CryptoHistory.jsx
+++ src/components/CryptoHistory/CryptoHistory.jsx
@@ -1,5 +1,30 @@
 import { BaseTable, THead, Th, Tr, Td } from './CryptoHistory.styled';
+import { format, parseISO } from 'date-fns';
 
-export const CryptoHistory = () => {
-  return <div>CryptoHistory</div>;
+export const CryptoHistory = ({ items }) => {
+  return (
+    <div>
+      <BaseTable>
+        <THead>
+          <tr>
+            <Th>№</Th>
+            <Th>PRICE</Th>
+            <Th>AMOUNT</Th>
+            <Th>DATE</Th>
+          </tr>
+        </THead>
+
+        <tbody>
+          {items.map(({ id, price, amount, date }, index) => (
+            <Tr key={id}>
+              <Td>{index + 1}</Td>
+              <Td>{price}</Td>
+              <Td>{amount}</Td>
+              <Td>{format(parseISO(date), 'Pp')}</Td>
+            </Tr>
+          ))}
+        </tbody>
+      </BaseTable>
+    </div>
+  );
 };
