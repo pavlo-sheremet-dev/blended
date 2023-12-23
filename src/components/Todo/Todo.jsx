79ce8e11923f41ab9c03f08ca41ee24c8701a5e7
KaterynaPryimakova <134732205+KaterynaPryimakova@users.noreply.github.com>
2023-12-23 20:25:15 +0200

    pryimakova

--- src/components/Todo/Todo.jsx
+++ src/components/Todo/Todo.jsx
@@ -2,6 +2,16 @@ import { Text } from '../Text/Text.styled';
 import { TodoWrapper, DeleteButton, EditButton } from './Todo.styled';
 import { RiDeleteBinLine, RiEdit2Line } from 'react-icons/ri';
 
-export const Todo = () => {
-  return <h2>Todo</h2>;
+export const Todo = ({ description, index, deleteTodo }) => {
+  return (
+    <TodoWrapper>
+      <Text textAlign="center" marginBottom="20px">
+        TODO {index}
+      </Text>
+      <Text>{description}</Text>
+      <DeleteButton type="button" onClick={deleteTodo}>
+        <RiDeleteBinLine size={24} />
+      </DeleteButton>
+    </TodoWrapper>
+  );
 };
--- src/tabs/Todos.jsx
+++ src/tabs/Todos.jsx
@@ -14,6 +14,20 @@ export class Todos extends Component {
     todos: [],
   };
 
+  componentDidMount() {
+    const data = JSON.parse(localStorage.getItem('todos'));
+    if (data) {
+      this.setState({ todos: data });
+    }
+  }
+
+  componentDidUpdate(_, prevState) {
+    if (prevState.todos !== this.state.todos) {
+      const data = JSON.stringify(this.state.todos);
+      localStorage.setItem('todos', data);
+    }
+  }
+
   addToDo = toDoText => {
     const todo = {
       text: toDoText,
@@ -24,11 +38,33 @@ export class Todos extends Component {
     });
   };
 
+  deleteTodo = todoId => {
+    this.setState(prevState => ({
+      todos: prevState.todos.filter(todo => todo.id !== todoId),
+    }));
+  };
+
   render() {
     return (
       <Container>
         <SearchForm getQuery={this.addToDo} />
-        <Text textAlign={'center'}>There are no any todos...</Text>
+        {this.state.todos.length > 0 ? (
+          <Grid>
+            {this.state.todos.map((todo, index) => {
+              return (
+                <GridItem key={todo.id}>
+                  <Todo
+                    description={todo.text}
+                    index={index + 1}
+                    deleteTodo={() => this.deleteTodo(todo.id)}
+                  />
+                </GridItem>
+              );
+            })}
+          </Grid>
+        ) : (
+          <Text textAlign={'center'}>There are no any todos...</Text>
+        )}
       </Container>
     );
   }
